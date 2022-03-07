23ae5cc9ee8494141d1abd8ee4deab7e7b3339e1
Grace Barbara <98533301+gracebarbara@users.noreply.github.com>
2022-03-07 01:49:23 +0000

    cosine and tangent

--- main.py
+++ main.py
@@ -7,41 +7,47 @@ import math
 # then you do not fully understand functions
 # and should review how they work or ask for help
 def drawSineCurve(dart):
-  dart.down()
   for x in range(-360, 360):
     y = math.sin(math.radians(x))
     dart.goto(x, y)
+  return dart
 
 def setupWindow(mywindow=None):
   wn = turtle.Screen()
   wn.bgcolor('lightblue')
-  wn.setworldcoordinates(-360, -1 , 360, 1)
+  wn.setworldcoordinates(-360, -1, 360, 1)
 
 def setupAxis(myturtle=None):
   dart = turtle.Turtle()
   dart.speed(1)
-  dart.down()
   dart.backward(360)
-  dart.up()
-  dart.goto(0,0)
-  dart.down()
-  dart.forward(360) 
-  dart.up()
-  dart.goto(0,0)
+  dart.forward(720)
+  dart.backward(360)
   dart.left(90)
-  dart.down()
   dart.forward(1)
-  dart.up()
-  dart.right(180)
-  dart.down()
+  dart.backward(2)
   dart.forward(1)
+  dart.left(90)
+  dart.forward(360)
+  return dart
+
+def drawCosineCurve(dart):
   dart.up()
+  dart.goto(-360,1)
+  dart.down()
+  for x in range(-360, 360):
+    y = math.cos(math.radians(x))
+    dart.goto(x, y)
+  return dart
 
-def drawCosineCurve(myturtle=None):
-  gs
-
-def drawTangentCurve(myturtle=None):
-  
+def drawTangentCurve(dart):
+  dart.up()
+  dart.goto(-360,0)
+  dart.down()
+  for x in range(-360, 360):
+    y = math.tan(math.radians(x))
+    dart.goto(x, y)
+  return dart
   
 ##########  Do Not Alter Any Code Past Here ########
 def main():
