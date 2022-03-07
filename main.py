35fdc69bd15d24f1550347bfc9c2758154380353
Grace Barbara <98533301+gracebarbara@users.noreply.github.com>
2022-03-07 00:46:46 +0000

    sine graph

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 import turtle
+import math 
 
 ########### Your Code here ##############
 # You should only have functions here
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
+def drawSineCurve(dart):
+  dart.down()
+  for x in range(-360, 360):
+    y = math.sin(math.radians(x))
+    dart.goto(x, y)
 
+def setupWindow(mywindow=None):
+  wn = turtle.Screen()
+  wn.bgcolor('lightblue')
+  wn.setworldcoordinates(-360, -1 , 360, 1)
 
+def setupAxis(myturtle=None):
+  dart = turtle.Turtle()
+  dart.speed(1)
+  dart.down()
+  dart.backward(360)
+  dart.up()
+  dart.goto(0,0)
+  dart.down()
+  dart.forward(360) 
+  dart.up()
+  dart.goto(0,0)
+  dart.left(90)
+  dart.down()
+  dart.forward(1)
+  dart.up()
+  dart.right(180)
+  dart.down()
+  dart.forward(1)
+  dart.up()
 
+def drawCosineCurve(myturtle=None):
+  gs
 
-
-
-
-
+def drawTangentCurve(myturtle=None):
+  
+  
 ##########  Do Not Alter Any Code Past Here ########
 def main():
     #Part A
