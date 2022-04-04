0e33f84e64e20722e3b7199d34fb173dd433daf9
Grace Barbara <98533301+gracebarbara@users.noreply.github.com>
2022-04-04 22:43:15 +0000

    cleaned and bday generator functs

--- README.md
+++ README.md
@@ -1,26 +1,24 @@
 #### CS 110
-# Chapter 4 - Lab - Functions
-
-### [Assignment Description](https://docs.google.com/document/d/1V20D_upUX4MO8YmskKlRB25Yu2pCEv3-h8z4EAfrSno/edit?usp=sharing)
+# Midterm - Python Programming
 
 ***
 
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- In order to draw a sine function with turtle, I first created a function that uses the math module and a for loop to iterate the angle from -360 to 360 and map out the sine curve. It was then necessary to alter the window of the screen using set world coordinates within a function so that there was more room to plot the values of the sine function. Then, I used another funtion to draw the axis using basic turtle commands within another function. After the sine function, I then added a cosine function and a tangent function by slightly altering the command to fit those functions. Finally, the prewritten main function called to each of the functions and allowed the axis and trig functions to be drawn. 
+What did you clean up?: Firstly, I changed all of the parameters of my functions to be named with default values. I improved on the dryness of my code by removing unnecessary return statements and by setting each of my parameters to default values I was able to remove unnecessary creation of variables for turtles and screens. Within the drawAxis function, though not necessarily more condensed(it is the same amount of lines), I made the code more readable and easier to follow. I made sure to make all of my variables not have single letter variable names. And finally, I fixed the parameters of the range within my for loops making them (360, 361) since the second value (361) is exclusive. 
 
-## GRACE DAYS
-Grace days used for this assignment: 0
+Summary of function(s) added: The first function I added (zodiacGenerator) takes the users input birthday and, using a for loop, determines their zodiac sign. The second function that I added (inputBirthday) asks for the users birthday, turns it into a number that is able to be used with the datetime module, and returns that value. The next function (daysUntilBirthday) is a function that can calculate the days between the input birthday and the current date. Using a for loop it determines if the date has already passed or not and then subtracts that value from the current date as necessary. The parameters on this function require a call to the inputBirthday function and the current date using the datetime module which I learned how to navigate using python docs. Each of these functions are then called to in the main function in order to run.
 
-Grace days remaining: 5/5
+Summary of Feature Added: In summation, I added two separate features that require three functions in total. The first feature determines the users zodiac sign and the second feature takes the users birthday and the current date and determines the amount of days until their next birthday. 
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
  None
 
 ## REFERENCES:
- https://docs.python.org/3/library/turtle.html
- https://docs.python.org/3/library/turtle.html?highlight=set%20world%20coordinates#turtle.setworldcoordinates
+ https://docs.python.org/3/library/datetime.html?highlight=date#datetime.date 
+ https://pythontic.com/datetime/datetime/date
+ https://realpython.com/python-datetime/
 
 ## MISCELLANEOUS COMMENTS:
  None
--- main.py
+++ main.py
@@ -1,69 +1,159 @@
 import turtle
 import math 
+import datetime
+from datetime import date
 
 ########### Your Code here ##############
 # You should only have functions here
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
-def drawSineCurve(dart):
-  for x in range(-360, 360):
-    y = math.sin(math.radians(x))
-    dart.goto(x, y)
-  return dart
+def drawSineCurve(myturtle=None):
+	myturtle.up()
+	myturtle.goto(-360, 0)
+	myturtle.down()
+	for x_coord in range(-360, 361):
+		y_coord = math.sin(math.radians(x_coord))
+		myturtle.goto(x_coord, y_coord)
 
 def setupWindow(mywindow=None):
-  wn = turtle.Screen()
-  wn.bgcolor('lightblue')
-  wn.setworldcoordinates(-360, -1, 360, 1)
+	mywindow.bgcolor('lightblue')
+	mywindow.setworldcoordinates(-360, -1, 360, 1)
 
 def setupAxis(myturtle=None):
-  dart = turtle.Turtle()
-  dart.speed(1)
-  dart.backward(360)
-  dart.forward(720)
-  dart.backward(360)
-  dart.left(90)
-  dart.forward(1)
-  dart.backward(2)
-  dart.forward(1)
-  dart.left(90)
-  dart.forward(360)
-  return dart
+	myturtle.speed(1)
+	myturtle.up()
+	myturtle.goto(-360, 0)
+	myturtle.down()
+	myturtle.goto(360, 0)
+	myturtle.up()
+	myturtle.goto(0, -1)
+	myturtle.down()
+	myturtle.goto(0, 1)
 
-def drawCosineCurve(dart):
-  dart.up()
-  dart.goto(-360,1)
-  dart.down()
-  for x in range(-360, 360):
-    y = math.cos(math.radians(x))
-    dart.goto(x, y)
-  return dart
+def drawCosineCurve(myturtle=None):
+	myturtle.up()
+	myturtle.goto(-360,1)
+	myturtle.down()
+	for x_coord in range(-360, 361):
+		y_coord = math.cos(math.radians(x_coord))
+		myturtle.goto(x_coord, y_coord)
 
-def drawTangentCurve(dart):
-  dart.up()
-  dart.goto(-360,0)
-  dart.down()
-  for x in range(-360, 360):
-    y = math.tan(math.radians(x))
-    dart.goto(x, y)
-  return dart
-  
+def drawTangentCurve(myturtle=None):
+	myturtle.up()
+	myturtle.goto(-360,0)
+	myturtle.down()
+	for x_coord in range(-360, 361):
+		y_coord = math.tan(math.radians(x_coord))
+		myturtle.goto(x_coord, y_coord)
+
+def zodiacGenerator():
+	print("Welcome to the Astrological Sign Generator!")
+	day = int(input("What is your birthday? (1-31): " ))
+	month = int(input("What is your birth month? (1-12): "))
+	if month == 1:
+		if day<20:
+			zodiacsign = 'Capricorn'
+		else:
+			zodiacsign = "Aquarius"
+	elif month == 2:
+		if day<19:
+			zodiacsign = 'Aquarius'
+		else:
+			zodiacsign = 'Pisces'
+	elif month == 3:
+		if day<21:
+			zodiacsign = 'Pisces'
+		else:
+			zodiacsign = 'Aries'
+	elif month == 4:
+		if day<19:
+			zodiacsign = 'Aries'
+		else:
+			zodiacsign = 'Taurus'
+	elif month == 5:
+		if day<20:
+			zodiacsign = 'Taurus'
+		else:
+			zodiacsign = 'Gemini'
+	elif month == 6:
+		if day<20:
+			zodiacsign = 'Gemini'
+		else:
+			zodiacsign = 'Cancer'
+	elif month == 7:
+		if day<22:
+			zodiacsign = 'Cancer'
+		else:
+			zodiacsign = 'Leo'
+	elif month == 8:
+		if day<22:
+			zodiacsign = 'Leo'
+		else:
+			zodiacsign = 'Virgo'
+	elif month == 9:
+		if day<22:
+			zodiacsign = 'Virgo'
+		else:
+			zodiacsign = 'Libra'
+	elif month == 10:
+		if day<22:
+			zodiacsign = 'Libra'
+		else:
+			zodiacsign = 'Scorpio'
+	elif month == 11:
+		if day<21:
+			zodiacsign = 'Scorpio'
+		else:
+			zodiacsign = 'Sagittarius'
+	elif month == 12:
+		if day<21:
+			zodiacsign = 'Sagittarius'
+		else:
+			zodiacsign = 'Scorpio'
+	print("Your Astrological Sign is: ", zodiacsign)
+	return zodiacsign
+
+def inputBirthday():
+	day = int(input("What is your birthday? (1-31): " ))
+	month = int(input("What is your birth month? (1-12): "))
+	year = int(input("What year were you born?: "))
+	birthday = date(year, month, day)
+	return birthday
+
+def daysUntilBirthday(birthday_date=0, current_date=0):
+	birthday_hasnt_passed = date(current_date.year, birthday_date.month, birthday_date.day)
+	birthday_has_passed = date(current_date.year+1, birthday_date.month, birthday_date.day)
+	if birthday_hasnt_passed > current_date:
+		days = (birthday_hasnt_passed - current_date).days
+		return days
+	else:
+		days = (birthday_has_passed - current_date).days
+		return days
+
 ##########  Do Not Alter Any Code Past Here ########
 def main():
     #Part A
-    wn = turtle.Screen()
-    wn.tracer(5)
-    dart = turtle.Turtle()
-    dart.speed(0)
-    drawSineCurve(dart)
+		wn = turtle.Screen()
+		wn.tracer(5)
+		dart = turtle.Turtle()
+		dart.speed(0)
+		drawSineCurve(dart)
 
     #Part B
-    setupWindow(wn)
-    setupAxis(dart)
-    dart.speed(0)
-    drawSineCurve(dart)
-    drawCosineCurve(dart)
-    drawTangentCurve(dart)
-    wn.exitonclick()
+		setupWindow(wn)
+		setupAxis(dart)
+		dart.speed(0)
+		drawSineCurve(dart)
+		drawCosineCurve(dart)
+		drawTangentCurve(dart)
+
+		#AstrologicalGenerator
+		zodiacGenerator()
+		print("Now we will calculate how many days there are until your next birthday!")
+		birthday_date = inputBirthday()
+		current_date = date.today()
+		days = daysUntilBirthday(birthday_date, current_date)
+		print("There are", days,"days until your birthday!")
+		
 main()
